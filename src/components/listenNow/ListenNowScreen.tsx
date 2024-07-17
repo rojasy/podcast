f09d8b0745658b385e8396ac31f77bcc8e3eb9ce
rojasy <rojasyngaiza01@gmail.com>
2024-07-17 17:05:12 +0300

    first commit and some graphql query

---
+++ src/components/listenNow/ListenNowScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
